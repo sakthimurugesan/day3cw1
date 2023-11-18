a67e65934490c2cbd1e97848cdb464c705a77ece
SAKTHI M <msakthi150@gmail.com>
2023-11-18 09:58:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const toggleMessage = () => {
+    setShowMessage(!showMessage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleMessage}>
+        {showMessage ? 'Hide Message' : 'Show Message'}
+      </button>
+      {showMessage && <p>Hi! How are You!!!</p>}
     </div>
   );
-}
+};
 
 export default App;
